52476f36bf9d088d509814718b7ae9e8f3c71cfb
tpham0322 <truongpham1173@gmail.com>
2026-08-21 09:38:02 -0400

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/shortly_website_project/",
   plugins: [
-    tailwindcss()
-  ]
+    tailwindcss(),
+  ],
 });
