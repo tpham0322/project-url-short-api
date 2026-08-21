5a012c63c50504345f3f8abd97c7ddd262c099d7
tpham0322 <truongpham1173@gmail.com>
2026-08-21 09:42:21 -0400

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/shortly_website_project/",
+  base: "/project-url-short-api/",
   plugins: [
     tailwindcss(),
   ],
