1a1c133a1eee8d8baee81af3bf7b7a5aed827793
tpham0322 <truongpham1173@gmail.com>
2026-09-20 11:44:53 -0400

    Deploy React version

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/project-url-short-api-react/",
   plugins: [
     react(),
     tailwindcss()
